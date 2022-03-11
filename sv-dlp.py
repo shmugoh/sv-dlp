664dd8d6fd446921a08cceebc832ad27d69ede80
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-03-10 21:39:29 -0500

    proof of concept oop

--- sv-dlp.py
+++ sv-dlp.py
@@ -1,6 +1,10 @@
 import os
 import typer
+from PIL import Image
+
 from extractor import *
+from postdownload import merge_tiles
+from concurrent.futures import ThreadPoolExecutor
 
 app = typer.Typer()
 
@@ -29,5 +33,26 @@ def goodbye(
 
 
 if __name__ == "__main__":
-    services = add_services('extractor')
-    print(google.is_trekker("jYxwHUdPuhm8NGfAH6y8IA"))
+    # services = add_services('extractor')
+    pano_id = google.get_pano_id(-38.737475753012156, -72.59654594112037)
+    print(pano_id)
+    zoom = google._find_max_zoom(pano_id["pano_id"]) // 2
+
+    max_axis = google._find_max_axis(pano_id["pano_id"], zoom)
+    tile_arr_url = google._build_tile_arr(pano_id["pano_id"], zoom, max_axis)
+
+    tiles_io = merge_tiles.download_tiles(tile_arr_url)
+
+    tile_io_array = [] 
+    for i in range(len(tiles_io)): tile_io_array.append(None)
+
+    thread_size = len(tiles_io)
+    with ThreadPoolExecutor(max_workers=thread_size) as threads:
+        thread_count = int(threads._thread_name_prefix[-1])
+        row_arr = tile_io_array[thread_count]
+
+        thread = threads.submit(merge_tiles.stich_row, row_arr)
+        tile_io_array[thread_count] = thread.result()
+    
+    final_image = merge_tiles.merge_rows(tile_io_array)
+    final_image.save("pano.png")
