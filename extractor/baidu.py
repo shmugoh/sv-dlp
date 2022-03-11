594b06b3700ebb45b76a2beca076f2972f153ed1
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-03-10 21:57:10 -0500

    fix urls

--- extractor/baidu.py
+++ extractor/baidu.py
@@ -5,18 +5,18 @@ import requests
 import json as j
 
 class urls:
-    def _build_pano_url(panoID, lat, lon):
+    def _build_pano_url(lat, lon):
         """
         Builds Baidu URL that obtains pano ID from coords.
         """
-        url = f"https://mapsv{randrange(0, 1)}.bdimg.com/?qt=qsdata&x={lat}&y={lon}"
+        url = f"https://mapsv{randrange(0, 2)}.bdimg.com/?qt=qsdata&x={lat}&y={lon}"
         return url
 
     def _build_tile_url(panoID, x, y, zoom=4):
         """
         Builds Baidu Tile URL.
         """
-        url = f"https://mapsv{randrange(0, 1)}.bdimg.com/?qt=pdata&sid={panoID}&pos={y}_{x}&z={zoom}"
+        url = f"https://mapsv{randrange(0, 2)}.bdimg.com/?qt=pdata&sid={panoID}&pos={y}_{x}&z={zoom}"
         return url
 
     def _build_metadata_url(panoID):
@@ -24,7 +24,7 @@ class urls:
         Builds Baidu URL that returns maximum zoom levels
         and older imagery,
         """
-        url = f"https://mapsv{randrange(0, 1)}.bdimg.com/?qt=sdata&sid={panoID}"
+        url = f"https://mapsv{randrange(0, 2)}.bdimg.com/?qt=sdata&sid={panoID}"
         return url
 
 class baidu:
@@ -50,19 +50,22 @@ class baidu:
 
         # x axis
         while True:
-            url = urls._build_tile_url(pano_id, zoom, x, y)
+            url = urls._build_tile_url(pano_id, x, y, zoom)
             r = requests.get(url).status_code
             match r:
                 case 200:
                     x_axis = x
                     x += 1
+                # case 404:
+                #     x += 1
+                #     continue # temp
                 case _:
                     x = 0
                     break
 
         # y axis
         while True:
-            url = urls._build_tile_url(pano_id, zoom, x, y)
+            url = urls._build_tile_url(pano_id, x, y, zoom)
             r = requests.get(url).status_code
             match r:
                 case 200:
@@ -84,16 +87,13 @@ class baidu:
 
         for y in range(0, int(axis_arr['y']) + 1):
             for x in range(axis_arr['x']):
-                url = urls._build_sv_url(pano_id, zoom, x, y)
+                url = urls._build_tile_url(pano_id, x, y, zoom)
                 arr[y].append(url)
         return arr
 
 if __name__ == "__main__":
-    pano_id = "jYxwHUdPuhm8NGfAH6y8IA"
-    max_zoom = google._find_max_zoom(pano_id)
-    half_zoom = max_zoom // 2
-    print(urls._build_sv_url(pano_id, half_zoom))
-    max_axis = google._find_max_axis(pano_id, half_zoom)
-    tile_arr = google._build_tile_arr(pano_id, half_zoom, max_axis)
-    print(tile_arr)
-    print(len(tile_arr))
+   pano_id = baidu.get_pano_id(39.900139527145846, 116.3958936511099)
+   zoom = baidu._get_max_zoom(pano_id)
+   axis = baidu._find_max_axis(pano_id, zoom)
+   tile_arr = baidu._build_tile_arr(pano_id, zoom, axis)
+   print(tile_arr)
