101713e56d9362681684b017c6f9524f668939bd
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-03-13 18:01:42 -0500

    add more metadata func, xml -> json

--- extractor/google.py
+++ extractor/google.py
@@ -28,7 +28,7 @@ class urls:
         zoom levels, maximum image size, etc.
         '''
         i = randrange(0, 3)
-        url = f'https://cbk{i}.google.com/cbk?output=xml&panoid={pano_id}'
+        url = f'https://cbk{i}.google.com/cbk?output=json&panoid={pano_id}'
         return url
 
     def _build_geophoto_meta_url(pano_id):
@@ -39,18 +39,6 @@ class urls:
         url = f'https://maps.googleapis.com/maps/api/js/GeoPhotoService.GetMetadata?pb=!1m5!1sapiv3!5sUS!11m2!1m1!1b0!2m2!1sen!2sUS!3m3!1m2!1e2!2s{pano_id}!4m6!1e1!1e2!1e3!1e4!1e8!1e6&callback=a'
         return url
 
-    def _cbk_to_dict(url: str) -> dict:
-        '''
-        Turn CBK to Python Dictionary format, as
-        the CBK call is outputed in a XML format.
-
-        Automatically a GET request is performed, so no need
-        to parse one.
-        '''
-        cbk_xml = requests.get(url).content
-        cbk_dict = xmltodict.parse(cbk_xml)
-        return cbk_dict
-
     def _build_short_url(pano_id) -> str:
         """
         Build API call URL that shorts an encoded URL.
@@ -60,109 +48,130 @@ class urls:
         url = f'https://www.google.com/maps/rpc/shorturl?pb=!1s{encoded_input}'
         return url
 
+class google:
+    def get_pano_id(lat, lon) -> dict:
+        """
+        Returns closest Google panorama ID to given parsed coordinates.
+        """
 
-def get_pano_id(lat, lon) -> dict:
-    """
-    Returns closest Google panorama ID to given parsed coordinates.
-    """
+        url = urls._build_pano_url(lat, lon)
+        json = requests.get(url).text
+        pans = re.findall(r'\[[0-9],"(.+?)"].+?,\[\[null,null,(.+?),(.+?)\]', json)
+        # formatting should be changed soon
+        pan = {
+            "pano_id": pans[0][0],
+            "lat": pans[0][1],
+            "lon": pans[0][2]
+        }
+        # when implementing -F command, it shall return various pano ids with the date
+        # though keep in mind duplicates should be fixed and removed
+        return pan
 
-    url = urls._build_pano_url(lat, lon)
-    json = requests.get(url).text
-    pans = re.findall(r'\[[0-9],"(.+?)"].+?,\[\[null,null,(.+?),(.+?)\]', json)
-    # formatting should be changed soon
-    pan = {
-        "pano_id": pans[0][0],
-        "lat": pans[0][1],
-        "lon": pans[0][2]
-    }
-    # when implementing -F command, it shall return various pano ids with the date
-    # though keep in mind duplicates should be fixed and removed
-    return pan
+    def get_coords(pano_id) -> float: # lul
+        url = urls._build_metadata_url(pano_id)
+        data = requests.get(url).json()
+        return data["Location"]["lat"], data["Location"]["lng"]
 
-def _find_max_zoom(pano_id):
-    """
-    Finds maximum available zoom from given panorama ID.
-    """
-    url = urls._build_metadata_url(pano_id)
-    data = urls._cbk_to_dict(url)
-    return int(data['panorama']['data_properties']['@num_zoom_levels'])
+    def _find_max_zoom(pano_id):
+        """
+        Finds maximum available zoom from given panorama ID.
+        """
+        url = urls._build_metadata_url(pano_id)
+        data = requests.get(url).json()
+        return int(data['Location']['zoomLevels'])
 
-def _find_max_axis(pano_id, zoom) -> dict["x", "y"]:
-    x = 0
-    y = 0
-    x_axis = []
-    y_axis = []
+    def _find_max_axis(pano_id, zoom) -> dict["x", "y"]:
+        x = 0
+        y = 0
+        x_axis = []
+        y_axis = []
 
-    # x axis
-    while True:
-        url = urls._build_tile_url(pano_id, zoom, x, y)
-        r = requests.get(url).status_code
-        match r:
-            case 200:
-                x_axis = x
-                x += 1
-            case _:
-                x = 0
-                break
-
-    # y axis
-    while True:
-        url = urls._build_tile_url(pano_id, zoom, x, y)
-        r = requests.get(url).status_code
-        match r:
-            case 200:
-                y_axis = y
-                y += 1
-            case _:
-                break
-
-    max_axis = {
-        "x": x_axis,
-        "y": y_axis
-    }
-    return max_axis
-
-def _build_tile_arr(pano_id, zoom, axis_arr):
-    arr = []
-    for i in range(int(axis_arr['y']) + 1):
-        arr.append([])
-
-    # print(len(arr))
-    for y in range(0, len(arr)):
-        for x in range(axis_arr['x'] + 1):
+        # x axis
+        while True:
             url = urls._build_tile_url(pano_id, zoom, x, y)
-            arr[y].append(url)
-    return arr
+            r = requests.get(url).status_code
+            match r:
+                case 200:
+                    x_axis = x
+                    x += 1
+                case _:
+                    x = 0
+                    break
 
-def is_trekker(pano_id) -> bool:
-    """
-    Returns if given panorama ID is
-    trekker or not. Might be useful
-    with the planned generator.
+        # y axis
+        while True:
+            url = urls._build_tile_url(pano_id, zoom, x, y)
+            r = requests.get(url).status_code
+            match r:
+                case 200:
+                    y_axis = y
+                    y += 1
+                case _:
+                    break
 
-    Thank you nur#2584 for guiding me out.
-    """
-    url = urls._build_geophoto_meta_url(pano_id)
-    json = j.loads(requests.get(url).content[12:-2])
-    return len(json[1][0][5][0][3][0][0][2]) > 3
+        max_axis = {
+            "x": x_axis,
+            "y": y_axis
+        }
+        return max_axis
 
-def short_url(pano_id):
-    """
-    Shorts panorama ID by using the
-    share function found on Google Maps
-    """
-    url = urls._build_short_url(pano_id)
-    json = j.loads(json = j.loads(requests.get(url).content[5:]))
-    return json[0]
+    def _build_tile_arr(pano_id, zoom, axis_arr):
+        arr = []
+        for i in range(int(axis_arr['y']) + 1):
+            arr.append([])
+
+        # print(len(arr))
+        for y in range(0, len(arr)):
+            for x in range(axis_arr['x'] + 1):
+                url = urls._build_tile_url(pano_id, zoom, x, y)
+                arr[y].append(url)
+        return arr
+
+    def get_image_date(pano_id) -> str:
+        '''
+        Returns image date from
+        CBK URL.
+        '''
+        url = urls._build_metadata_url(pano_id)
+        data = requests.get(url).json()
+        return data["Data"]["image_date"]
+
+    def is_trekker(pano_id) -> bool:
+        """
+        Returns if given panorama ID is
+        trekker or not. Might be useful
+        with the planned generator.
+
+        Thank you nur#2584 for guiding me out.
+        """
+        url = urls._build_metadata_url(pano_id)
+        data = requests.get(url).json()
+        print(data["Data"])
+        data["Data"]["scene"] = 0
+
+        if int(data["Data"]["scene"]) == 1:
+            return True
+        elif int(data["Data"]["imagery_type"]) == 5 or "level_id" in ["Location"]:
+            return True
+        else: return False
+
+    def get_metadata(pano_id) -> str:
+        '''
+        Returns metadata from CBK url.
+        '''
+        url = urls._build_metadata_url(pano_id)
+        data = requests.get(url).json()
+        return data
+
+    def short_url(pano_id):
+        """
+        Shorts panorama ID by using the
+        share function found on Google Maps
+        """
+        url = urls._build_short_url(pano_id)
+        json = j.loads(json = j.loads(requests.get(url).content[5:]))
+        return json[0]
 
 
 if __name__ == "__main__":
-    print(urls._build_pano_url(50.95475387573242,6.971647262573242))
-    pano_id = "jYxwHUdPuhm8NGfAH6y8IA"
-    max_zoom = _find_max_zoom(pano_id)
-    half_zoom = max_zoom // 2
-    print(urls._build_tile_url(pano_id, half_zoom))
-    max_axis = _find_max_axis(pano_id, half_zoom)
-    tile_arr = _build_tile_arr(pano_id, half_zoom, max_axis)
-    print(tile_arr)
-    print(len(tile_arr))
+    print(google.is_trekker("Ho8umHQ4dGWqNs1glFHYcQ"))
