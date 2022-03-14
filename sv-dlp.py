18e6e1779c30f5a67d246708e644664d3f7d3e0a
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-03-13 19:21:25 -0500

    add CLI support
    honestly this is looking too awful and i might consider using argparser instead cause of how horrible typer's documentation is

--- sv-dlp.py
+++ sv-dlp.py
@@ -1,48 +1,88 @@
-import os
+from pprint import pprint
+import sys
 import typer
-from PIL import Image
-
 from extractor import *
+from extractor import baidu
 from postdownload import merge_tiles
 
 app = typer.Typer()
 
-def add_services(folder):
-    service_arr = os.listdir(folder)
-    for i in range(len(service_arr) -1):
-        if service_arr[i][-3:] != '.py':
-            service_arr.pop(i)
-    return service_arr
+def _is_coord(input):
+    if ',' in input: return True
+    else: return False
 
 @app.command()
-def hello(
-    name: str):
+def metadata(
+    pano: str,
+    get_coords: bool=typer.Option(False, "--get-coords"),
+    get_date: bool=typer.Option(False, "--get-date"),
+    get_metadata: bool=typer.Option(False, "--get-metadata")):
 
-    typer.echo(f"Hello {name}")
+    metadata_flags = {
+        "coords": get_coords,
+        "date": get_date,
+        "metadata": get_metadata
+    }
+
+    is_coord = _is_coord(pano)
+    if is_coord:
+        pano = pano.split(',')
+        lat, lon = [float(i) for i in pano]
+        pano = google.get_pano_id(lat, lon)['pano_id']
+
+    if "True" in str(metadata_flags):
+        for i in metadata_flags:
+            if metadata_flags[i] == True:
+
+                match i:
+                    case "coords":
+                        typer.echo(google.get_coords(pano))
+                    case "date":
+                        typer.echo(google.get_image_date(pano))
+                    case "metadata":
+                        typer.echo(pprint(google.get_metadata(pano)))
+                    case "trekker":
+                        typer.echo(google.is_trekker(pano))
 
 @app.command()
-def goodbye(
-    name: str,
-    formal: bool = False):
+def download(
+    pano: str,
+    service: str='google',
+    zoom: int=None):
 
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+    is_coord = _is_coord(pano)
+    if is_coord:
+        pano = pano.split(',')
+        lat, lon = [float(i) for i in pano]
 
+    match service:
+        case "google":
+            if lat and lon:
+                pano_id = google.get_pano_id(lat, lon)['pano_id']
+            if zoom == None:
+                zoom = google._find_max_zoom(pano_id) // 2
 
-if __name__ == "__main__":
-    # services = add_services('extractor')
-    pano_id = google.get_pano_id(4.662278433183548, -74.11502894958988)["pano_id"]
-    print(pano_id)
-    # zoom = google._find_max_zoom(pano_id)
-    zoom = 5
+            max_axis = google._find_max_axis(pano_id, zoom)
+            tile_arr_url = google._build_tile_arr(pano_id, zoom, max_axis)
+        case "bing":
+            typer.echo("bing")
+        case "baidu":
+            if lat and lon:
+                pano_id = baidu.get_pano_id(lat, lon)
+            if zoom == None:
+                zoom = baidu._get_max_zoom(pano_id)
 
-    max_axis = google._find_max_axis(pano_id, zoom)
-    tile_arr_url = google._build_tile_arr(pano_id, zoom, max_axis)
-    print(tile_arr_url)
+            print(pano_id)
+            max_axis = baidu._find_max_axis(pano_id, zoom)
+            tile_arr_url = baidu._build_tile_arr(pano_id, zoom, max_axis)
+        case _:
+            typer.echo("Invalid service")
+            raise typer.Exit()
+
+    typer.echo("Downloading tiles...")
     tiles_io = merge_tiles.download_tiles(tile_arr_url)
 
+    typer.echo("Merging tiles...")
     tile_io_array = []
     for row in tiles_io:
         buff = merge_tiles.stich_row(row)
@@ -50,3 +90,7 @@ if __name__ == "__main__":
 
     final_image = merge_tiles.merge_rows(tile_io_array)
     final_image.save(f'{pano_id}.png')
+    typer.echo("Done")
+
+if __name__ == "__main__":
+    app()
