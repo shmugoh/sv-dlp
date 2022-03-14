51bc2ff5a1b369745f779f388ee201c7dfd9649a
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-03-14 17:57:20 -0500

    fix bounding box and add tile_arr

--- extractor/bing.py
+++ extractor/bing.py
@@ -45,11 +45,11 @@ def _get_bounding_box(lat, lon):
     pi = math.pi
     eSq = 0.00669437999014 # eccentricity squared
     a = 6378137.0 # equatorial radius
-    lat = lat * pi / 180
-    lon = lon * pi / 180
+    lat_p = lat * pi / 180
+    lon_p = lon * pi / 180
 
-    lat_len = (pi * a * (1 - eSq)) / (180 * math.pow((1 - eSq * math.pow(math.sin(lat), 2)), 3 / 2))
-    lon_len = (pi * a * math.cos(lon)) / (180 * math.sqrt((1 - (eSq * math.pow(math.sin(lon), 2)))))
+    lat_len = (pi * a * (1 - eSq)) / (180 * math.pow((1 - eSq * math.pow(math.sin(lat_p), 2)), 3 / 2))
+    lon_len = (pi * a * math.cos(lon_p)) / (180 * math.sqrt((1 - (eSq * math.pow(math.sin(lon_p), 2)))))
 
     bounds = {
         "north": lat + (1000 / lat_len),
@@ -76,7 +76,7 @@ def _find_axis(base4_bubble, zoom=2):
     arr = []
     for i in range(0, 6):
         arr.append([])
-    print(arr)
+    # print(arr)
 
     max_tiles = int(math.pow(4, zoom))
     for tile_id in range(0, 6):
@@ -87,11 +87,23 @@ def _find_axis(base4_bubble, zoom=2):
             else:
                 subdiv_base4 = urls._base4(group).zfill(zoom)
             tile_pos = f"{tile_id_base4}{subdiv_base4}"
-            print(f"hs{base4_bubble}{tile_pos}", tile_id_base4, group)
+            # print(f"hs{base4_bubble}{tile_pos}", tile_id_base4, group)
             arr[tile_id].append(tile_pos)
 
     return arr
 
+def _build_tile_arr(bubble, axis_arr):
+        arr = []
+        for i in range(len(axis_arr)): arr.append([])
+        print(arr)
+
+        for i in axis_arr:
+            count = axis_arr.index(i)
+            for pos in i:
+                print(pos)
+                arr[count].append(urls._build_tile_url(bubble, pos))
+
+        return arr
 
 def get_bubble(bounds):
     """
@@ -99,11 +111,12 @@ def get_bubble(bounds):
     with parsed coordinate bounds.
     """
 
-    url = urls._build_tile_url(bounds['north'], bounds['south'], bounds['east'], bounds['west'])
+    url = urls._build_pano_url(bounds['north'], bounds['south'], bounds['east'], bounds['west'])
     json = requests.get(url).json()
-
+    # print(json)
     bubble_id = json[1]["id"]
     base4_bubbleid = urls._base4(bubble_id)
+    print(bubble_id)
 
     bubble = {
         "bubble_id": bubble_id,
@@ -127,4 +140,10 @@ def get_bubble(bounds):
 # -70.635805
 
 if __name__ == '__main__':
-    _find_axis(urls._base4(118031601).zfill(16))
+    lat = -33.590928
+    lng = -70.715060
+    bounds = _get_bounding_box(lat, lng)
+    bubble_id = get_bubble(bounds)['base4_bubble']
+    axis = _find_axis(bubble_id)
+    print(_build_tile_arr(bubble_id, axis))
+    # bubble_id = get_bubble(bounds)
