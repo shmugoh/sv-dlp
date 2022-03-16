b6a3ff1d56265cd8ee0df4c150a628c4cf9a3aeb
Juan <juan@DESKTOP-4DDMTRJ.localdomain>
2022-03-16 12:31:15 -0500

    add help for each argument

--- sv-dlp.py
+++ sv-dlp.py
@@ -1,13 +1,17 @@
 import argparse
+from time import sleep
 
 from extractor.google import google
+from extractor.bing import bing
+from extractor.baidu import baidu
+
 from postdownload import merge_tiles
 
 parser = argparse.ArgumentParser(
     description='''
     Download Street View panoramas from various services,
     obtain metadata and short links.
-    '''
+    ''',
 )
 
 '''
@@ -17,14 +21,14 @@ parser.add_argument(
     'pano',
     metavar='pano',
     nargs="+",
-    # help='an integer for the accumulator'
+    help='input to scrape from. can be panorama ID or coordinates'
 )
 parser.add_argument(
     '-s', '--service',
     metavar='service',
     nargs=1,
     default=['google'],
-    # help='an integer for the accumulator'
+    help='service to scrape from'
 )
 parser.add_argument(
     '-z', '--zoom',
@@ -34,18 +38,19 @@ parser.add_argument(
     default=[0],
     # help='an integer for the accumulator'
 )
-
 args = parser.parse_args()
 
 def _is_coord(coords):
     for coord in coords:
-        if float(coord):
+        if float(coord[:-1]):
             return True
         else: return False
 
 def download_panorama(tile_arr_url, keep_tiles=False):
+    print("Downloading Tiles...")
     tiles_io = merge_tiles.download_tiles(tile_arr_url)
 
+    print("Merging tiles...")
     tile_io_array = []
     for row in tiles_io:
         buff = merge_tiles.stich_row(row)
@@ -54,25 +59,41 @@ def download_panorama(tile_arr_url, keep_tiles=False):
     img = merge_tiles.merge_rows(tile_io_array)
     return img
 
+if args.pano:
+    pano = args.pano
+    if _is_coord(pano):
+        lat = float(pano[0][:-1])
+        lng = float(pano[1])
+
+print("Obtaining Tile URLs...")
 match args.service[0]:
     case 'google':
-        pano = args.pano
-        if _is_coord(pano):
-           lat = float(pano[0])
-           lng = float(pano[1])
-           pano = google.get_pano_id(lat, lng)["pano_id"]
-           print(pano)
+        if lat and lng:
+            print("Getting Panorama ID...")
+            pano = google.get_pano_id(lat, lng)["pano_id"]
+        # print(pano)
         if args.zoom == [0]:
             zoom = google._find_max_zoom(pano) // 2
 
         max_axis = google._find_max_axis(pano, zoom)
         tile_arr_url = google._build_tile_arr(pano, zoom, max_axis)
     case 'bing':
-        print("Lul")
+        if lat and lng:
+            bounds = bing._get_bounding_box(lat, lng)
+            pano = bing.get_bubble(bounds)['base4_bubble']
+        
+        max_axis = bing._find_axis(pano)
+        tile_arr_url = bing._build_tile_arr(pano, max_axis)
     case 'baidu':
-        print("Lul")
+        if lat and lng:
+            pano = baidu.get_pano_id(lat, lng)
+        if args.zoom == [0]:
+            zoom = baidu._find_max_zoom(pano) // 2
+        
+        max_axis = baidu._find_max_axis(pano, zoom)
+        tile_arr_url = bing._build_tile_arr(pano, zoom, max_axis)
     case _:
-        print("invalid service")
+        print("Invalid service")
 
 img = download_panorama(tile_arr_url)
 img.save(f"{pano}.png")
