ffd59f5f2690cab3e91899561ed074dbe3af767b
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-04-15 18:26:07 -0500

    Create build.py

---
+++ build.py
@@ -0,0 +1,16 @@
+import PyInstaller.__main__
+import platform
+
+SYS_OS = platform.system()
+SYS_ARCH = platform.architecture()[0][:2]
+suffix = f"{'_macos' if SYS_OS == 'Darwin' else ''}{'_x86' if SYS_ARCH == '32' else ''}{'.exe' if SYS_OS == 'Windows' else ''}"
+
+def main():
+    opts = [
+        'sv-dlp.py',
+        '--onefile',
+        f'--name=sv-dlp{suffix}'
+    ]
+
+    print(f"Building sv-dlp for platform {SYS_OS} with architecture {SYS_ARCH[0]}")
+    PyInstaller.__main__.run(opts)
