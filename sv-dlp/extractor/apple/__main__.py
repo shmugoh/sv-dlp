b9ab71c64a097770e525d9209a2d43fff62e7797
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-07-27 19:32:21 -0500

    [APPLE] Implement URL and Auth
    ...and it works!
    now we gotta figure out how to obtain the panorama id from the coordinates

--- sv-dlp/extractor/apple/__main__.py
+++ sv-dlp/extractor/apple/__main__.py
@@ -1,9 +1,10 @@
 import requests
-import extractor
+# import extractor
+from auth import Authenticator
 
 class urls:
-    def _build_tile_url(pano_id, x, y, zoom):
-        url = f"https://example.com/?pano={pano_id}&pos={x}_{y}&zoom={zoom}"
+    def _build_tile_url(pano_id, face, zoom):
+        url = f"https://gspe72-ssl.ls.apple.com/mnn_us/0665/1337/7579/4483/3546/{pano_id}/t/{face}/{zoom}"
         return url
 
     def _build_pano_url(lat, lon):
@@ -14,66 +15,52 @@ class urls:
         url = f"https://example.com/?pano={pano_id}"
         return url
 
-    def _build_short_url(pano_id) -> str:
-        raise extractor.ServiceNotSupported
+#     def _build_short_url(pano_id) -> str:
+#         raise extractor.ServiceNotSupported
 
-class misc:
-    def get_pano_from_url(url):
-        raise extractor.ServiceNotSupported
+# class misc:
+#     def get_pano_from_url(url):
+#         raise extractor.ServiceNotSupported
 
-    def short_url(pano_id):
-        raise extractor.ServiceNotSupported
+#     def short_url(pano_id):
+#         raise extractor.ServiceNotSupported
 
-class metadata:
-    def get_date(pano_id) -> str:
-        raise extractor.ServiceNotSupported
+# class metadata:
+#     def get_date(pano_id) -> str:
+#         raise extractor.ServiceNotSupported
 
-    def get_metadata(pano_id) -> str:
-        raise extractor.ServiceNotSupported
+#     def get_metadata(pano_id) -> str:
+#         raise extractor.ServiceNotSupported
 
-    def get_coords(pano_id) -> float:
-        raise extractor.ServiceNotSupported
+#     def get_coords(pano_id) -> float:
+#         raise extractor.ServiceNotSupported
 
-    def get_gen(pano_id):
-        raise extractor.ServiceNotSupported
+#     def get_gen(pano_id):
+#         raise extractor.ServiceNotSupported
 
-def get_pano_id(lat, lon):
-    raise extractor.ServiceNotSupported
+# def get_pano_id(lat, lon):
+#     raise extractor.ServiceNotSupported
 
-def get_max_zoom(pano_id):
-    raise extractor.ServiceNotSupported
+# def get_max_zoom(pano_id):
+#     raise extractor.ServiceNotSupported
 
 # last tow funcs are bit universal-ish,
 # so they could work with any service
-def _build_tile_arr(pano_id, zoom):
+def _build_tile_arr(pano_id, zoom=0):
+    auth = Authenticator()
     arr = []
-    x_y = [0, 0]
     i = 0
-
     while True:
-        if i >= 2:
-            break
-
-        if i == 0: url = urls._build_tile_url(pano_id, zoom, x_y[0], 0)
-        else: url = urls._build_tile_url(pano_id, zoom, 0, x_y[1])
-
-        r = requests.get(url).status_code
-        match r:
-            case 200:
-                x_y[i] += 1
-            case _:
-                i += 1
-                continue
-
-    for y in range(int(x_y[1])):
-        arr.append([])
-        for x in range(x_y[0]):
-            url = urls._build_tile_url(pano_id, zoom, x, y)
-            arr[y].insert(x, url)
+        url = auth.authenticate_url(urls._build_tile_url(pano_id, i, zoom))
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            i += 1
+            arr.append(url)
+        else: break
     return arr
 
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    print(_build_tile_arr(1095101453))
 #    pano_id = get_pano_id(39.900139527145846, 116.3958936511099)
 #    zoom = _get_max_zoom(pano_id)
 #    axis = _find_max_axis(pano_id, zoom)
