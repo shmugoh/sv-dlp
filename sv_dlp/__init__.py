2bb98fb24e23a8f78b43cea2b0716e77bc0c178a
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-08-15 15:34:08 -0500

    [MODULE] Rename pano_id

--- sv_dlp/__init__.py
+++ sv_dlp/__init__.py
@@ -9,27 +9,28 @@ class sv_dlp:
     def __init__(self, service="google"):
         self.service = getattr(services, service)
         self.service_str = service
-        self.pano = None
+        self.pano_id = None
         self.metadata = None
         pass
-    def download_panorama(self, pano=None, zoom=3, lat=None, lng=None) -> Image:
+
+    def download_panorama(self, pano_id=None, zoom=3, lat=None, lng=None) -> Image:
         if lat and lng:
-            self.pano = self.get_pano_id(lat, lng)
-        elif self.pano is None:
-            self.pano = pano
+            self.pano_id = self.get_pano_id(lat, lng)
+        elif self.pano_id is None:
+            self.pano_id = pano_id
         img = download.panorama(self.pano, zoom, self.service)
         return img
-    def get_metadata(self, pano=None, lat=None, lng=None) -> list:
-        md = self.service.metadata.get_metadata(pano=pano, lat=lat, lng=lng)
+
+    def get_metadata(self, pano_id=None, lat=None, lng=None) -> list:
+        md = self.service.metadata.get_metadata(pano_id=pano_id, lat=lat, lng=lng)
         self.metadata = md
         return md
     def get_pano_id(self, lat, lng) -> str:
-        if self.metadata:
-            pano = self.metadata["pano_id"]
-        else:
-            pano = self.service.get_pano_id(lat, lng)
-        self.pano = pano
-        return pano
+        if self.metadata is None:
+            self.get_metadata(lat=lat, lng=lng)
+        pano_id = self.metadata["pano_id"]
+        self.pano_id = pano_id
+        return pano_id
     # def get_linked_panos(self) -> str:
     #     sv_dlp.get_metadata(self, pano=None, lat=None, lng=None)
     #     return self.metadata['near_panos']
