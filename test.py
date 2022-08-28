5261ad99a7bb5ce2f4086dd0d17c15b17dca10bb
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2022-08-28 17:59:19 -0500

    [Build] Add Test Script

---
+++ test.py
@@ -0,0 +1,41 @@
+import sv_dlp
+from pprint import pprint
+sv_dlp = sv_dlp.sv_dlp()
+
+def west_test():
+    lat, lng = 37.77499382574212, -122.47185699855395 # Apple, Bing & Google
+    available_services = sv_dlp.get_available_services(lat, lng)
+
+    for service in available_services:
+        _test(lat=lat, lng=lng, service=service)
+
+def baidu_test():
+    lat, lng = 39.90802391019931, 116.3403752455185
+    _test(lat=lat, lng=lng, service="baidu")
+
+def yandex_test():
+    lat, lng = 55.76550473786485, 37.54340745542864
+    _test(lat=lat, lng=lng, service="yandex")
+
+def _test(lat, lng, service=""):
+    print(service)
+    sv_dlp.set_service(service)
+
+    md = sv_dlp.get_metadata(lat=lat, lng=lng)
+    pano = sv_dlp.get_pano_id(lat, lng)
+    print(f"{md}\n\n{pano}")
+    print("Downloading...")
+    img = sv_dlp.download_panorama(pano_id=pano)
+    print("Saving...")
+    sv_dlp.postdownload.save_panorama(img, md)
+    print("--------------------------------")
+
+if __name__ == "__main__":
+    print("West Services Test...")
+    west_test()
+
+    print("Baidu Service Test...")
+    baidu_test()
+    
+    print("Yandex Test...")
+    yandex_test()
