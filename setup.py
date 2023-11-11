9acf559f26e1b16e9f97ae9ac450a914276539dd
Juan <juan@shmugo.co>
2023-11-11 15:02:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Distribution
+from setuptools.command.build_ext import build_ext
 from utils.version import read_file, read_version
+import platform
 
 VERSION = read_version()
 REQUIREMENTS = read_file('requirements.txt').splitlines()
@@ -7,6 +9,24 @@ DESCRIPTION = '''
 Flexible Street View API Wrapper for Python
 '''
 LONG_DESCRIPTION = read_file('README.md')
+
+# Define the data files to include
+data_files = []
+
+# Get the current platform
+current_platform = platform.system()
+
+# Include the appropriate binary
+if current_platform == "Windows":
+    data_files.append(('bin', ['dist/bin/sv-dlp.exe']))
+elif current_platform == "Linux":
+    data_files.append(('bin', ['dist/bin/sv-dlp']))
+elif current_platform == "Darwin":
+    data_files.append(('bin', ['dist/bin/sv-dlp']))
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
 setup(
     name='sv-dlp',
@@ -23,4 +43,6 @@ setup(
     python_requires='>=3.10', # thinking about making it 3.8
     install_requires=REQUIREMENTS,
     include_package_data=True,
+    data_files=data_files,  # Add the data files
+    distclass=BinaryDistribution,
 )
