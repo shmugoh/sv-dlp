ae5a783e78876958891c263067f00d8004a94228
Juan <juan@shmugo.co>
2023-11-12 11:54:24 -0500

    [DEV] Improve URL UnitTest & Specific Service Test

--- utils/test.py
+++ utils/test.py
@@ -42,6 +42,7 @@ class TestSvDlp(unittest.TestCase):
             "yandex": ("https://yandex.com/maps/213/moscow/?l=stv%2Csta&ll=37.600082%2C55.790894&mode=search&panorama%5Bdirection%5D=286.651637%2C-5.390625&panorama%5Bfull%5D=true&panorama%5Bid%5D=1298034314_672832338_23_1528540830&panorama%5Bpoint%5D=37.599959%2C55.790842&panorama%5Bspan%5D=105.239256%2C60.000000&sll=37.600050%2C55.790854&text=55.790854%2C37.600050&z=20", 41.3432002141999, 69.2646005452),
             "navae": ("https://naver.me/5nPJ8YmO", 37.51939426170934, 126.96017727550505)
         }
+        # TODO: Test w/ new URLs instead of Constant, as these expire shortly after.
 
     def test_pano_download(self):
         """
@@ -74,8 +75,7 @@ class TestSvDlp(unittest.TestCase):
                 try:
                     self.sv_dlp.set_service(service)
                     md = self.sv_dlp.get_metadata(lat=lat, lng=lng)
-                    pano = self.sv_dlp.get_pano_id(lat, lng)
-                    assert pano is not None, "Failed to get panorama ID"
+                    assert md is not None, "Failed to get panorama ID"
                     print(wrapper.fill(str(md)))
                 except Exception as e:
                     self.fail(f"ERROR: LL Test - {service}")
@@ -89,17 +89,71 @@ class TestSvDlp(unittest.TestCase):
                 with self.subTest(service=service):
                     print(f"URL Test: {service}")
                     try:
-                        self.sv_dlp.set_service(service)
-                        url, lat, lng = self.url_dict[service]
+                        url, lat, lng = self.url_dict[self.service]
                         pano = self.sv_dlp.get_pano_from_url(url)
+                        print(f"Pano from URL: {pano}")
                         assert pano is not None, "Failed to get panorama from URL"
+                        
                         url = self.sv_dlp.short_url(pano_id=pano, heading=-randrange(-360, 360), pitch=randrange(-90, 90), zoom=randrange(10, 100))
                         assert url is not None, "Failed to get short URL"
+                        print(f"Pano URL: {url}")
+                        
                         c_url = self.sv_dlp.short_url(lat=lat, lng=lng, heading=-randrange(-360, 360), pitch=randrange(-90, 90), zoom=randrange(10, 100))
                         assert c_url is not None, "Failed to get short URL from coordinates"
-                        print(f"{service}: {url}")
+                        print(f"Coord URL: {c_url}")
                     except Exception as e:
                         self.fail(f"ERROR: URL Test - {service}")
+
+    def test_service(self):
+        """
+        Tests Download, Metadata, and URL functions for parsed service.
+        """
+        import os
+        self.service = os.environ["SERVICE"]
+        if self.service not in self.services:
+            self.fail("Invalid Service")
+            
+        # Initiate Service & Key Vars
+        self.sv_dlp.set_service(self.service)
+        lat, lng, pano_id = self.services[self.service]
+        
+        # Panorama Download Service
+        print(f"{self.service} - Panorama Download Service")
+        with self.subTest():
+            if self.service == "apple" or self.service == "bing":
+                md = self.sv_dlp.get_metadata(lat=lat, lng=lng)
+                pano_id = md.pano_id
+            else:
+                md = self.sv_dlp.get_metadata(pano_id)
+            img = self.sv_dlp.download_panorama(pano_id=pano_id)
+            self.sv_dlp.postdownload.save_panorama(img, md)
+            assert img is not None, "Failed to download panorama"                
+
+        # Metadata Service
+        print(f"{self.service} - Metadata Service")
+        with self.subTest():
+            md = self.sv_dlp.get_metadata(lat=lat, lng=lng)
+            assert md is not None, "Failed to get metadata"
+            print(md)
+        
+        # URL Service
+        print(f"{self.service} - URL Service")
+        with self.subTest():
+                if self.service not in self.url_dict:
+                    self.skipTest(f"{self.service} does not support URL Service")
+
+                url, lat, lng = self.url_dict[self.service]
+                pano = self.sv_dlp.get_pano_from_url(url)
+                print(f"Pano from URL: {pano}")
+                assert pano is not None, "Failed to get panorama from URL"
+                
+                url = self.sv_dlp.short_url(pano_id=pano, heading=-randrange(-360, 360), pitch=randrange(-90, 90), zoom=randrange(10, 100))
+                assert url is not None, "Failed to get short URL"
+                print(f"Pano URL: {url}")
+                
+                c_url = self.sv_dlp.short_url(lat=lat, lng=lng, heading=-randrange(-360, 360), pitch=randrange(-90, 90), zoom=randrange(10, 100))
+                assert c_url is not None, "Failed to get short URL from coordinates"
+                print(f"Coord URL: {c_url}")
 
 if __name__ == '__main__':
     unittest.main()
